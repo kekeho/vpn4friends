4ef5dec2c6cbe9ce489a640271b6e5f343c4791c
Hiroki.T <hirodora@me.com>
2019-04-11 23:31:49 +0900

    change: openvpn_server using local dockerfile

--- createuser.sh
+++ createuser.sh
@@ -1,5 +1,5 @@
 echo -n "Input client name: "
 read CLIENT_NAME
 
-docker run -v $PWD/vpn_server/openvpn_data:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn easyrsa build-client-full ${CLIENT_NAME} nopass
-docker run -v $PWD/vpn_server/openvpn_data:/etc/openvpn --log-driver=none --rm kylemanna/openvpn ovpn_getclient ${CLIENT_NAME} > ${CLIENT_NAME}.ovpn
+docker run -v $PWD/vpn_server/openvpn_data:/etc/openvpn --log-driver=none --rm -it vpn4friends_openvpn_server easyrsa build-client-full ${CLIENT_NAME} nopass
+docker run -v $PWD/vpn_server/openvpn_data:/etc/openvpn --log-driver=none --rm vpn4friends_openvpn_server ovpn_getclient ${CLIENT_NAME} > ${CLIENT_NAME}.ovpn
