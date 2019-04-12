f944e5ffb62ab07cdcf02d098f37033d158a121d
Hiroki.T <hirodora@me.com>
2019-04-12 22:23:53 +0900

    add: generate ovpn profile

--- vpn_server/accounts_server/accounts/urls.py
+++ vpn_server/accounts_server/accounts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('profile_create', views.profile_create),
 ]
--- vpn_server/accounts_server/accounts/views.py
+++ vpn_server/accounts_server/accounts/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.shortcuts import HttpResponse
+import subprocess
+import os
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def profile_create(request):
+    if request.method != 'POST':
+        return HttpResponse(status=405)
+
+    email = request.POST['email']
+
+    echo_pipe = subprocess.Popen(['echo', os.environ['CA_PASS']], stdout=subprocess.PIPE)
+    easyrsa_pipe = subprocess.Popen(['easyrsa', 'build-client-full', email, 'nopass'], stdin=echo_pipe.stdout)
+    echo_pipe.stdout.close()
+    result = easyrsa_pipe.communicate()[0]
+    print(result)
+    return redirect('/connect')
