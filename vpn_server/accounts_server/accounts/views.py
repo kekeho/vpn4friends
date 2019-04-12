f83a90db112cbc66dfd1ec2d6249864811c3e888
Hiroki.T <hirodora@me.com>
2019-04-13 00:14:59 +0900

    add: save profile on server

--- vpn_server/accounts_server/accounts/views.py
+++ vpn_server/accounts_server/accounts/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import HttpResponse
 import subprocess
 import os
 
+PROFILES_DIR = '/code/profiles'
+
 
 def index(request):
     return render(request, 'index.html')
@@ -14,9 +16,18 @@ def profile_create(request):
 
     email = request.POST['email']
 
-    echo_pipe = subprocess.Popen(['echo', os.environ['CA_PASS']], stdout=subprocess.PIPE)
-    easyrsa_pipe = subprocess.Popen(['easyrsa', 'build-client-full', email, 'nopass'], stdin=echo_pipe.stdout)
-    echo_pipe.stdout.close()
-    result = easyrsa_pipe.communicate()[0]
-    print(result)
+    if not os.path.exists(f'{PROFILES_DIR}/{email}.ovpn'):
+        # Generate Profile
+        echo_pipe = subprocess.Popen(['echo', os.environ['CA_PASS']], stdout=subprocess.PIPE)
+        easyrsa_pipe = subprocess.Popen(['easyrsa', 'build-client-full', email, 'nopass'], stdin=echo_pipe.stdout)
+        echo_pipe.stdout.close()
+        result = easyrsa_pipe.communicate()[0]
+        print(result)
+
+        # Save profile
+        content = subprocess.check_output(['ovpn_getclient', email])
+
+        with open(f'{PROFILES_DIR}/{email}.ovpn', 'w') as profile:
+            profile.write(content.decode())
+
     return redirect('/connect')
