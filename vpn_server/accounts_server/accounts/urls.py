979199a9c9e9ea59761b2680aa26fad6f2f2418c
Hiroki.T <hirodora@me.com>
2019-04-13 20:28:15 +0900

    fix: index url

--- vpn_server/accounts_server/accounts/urls.py
+++ vpn_server/accounts_server/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.index),
+    path('', views.index),
     path('profile_create/', views.profile_create),
     path('connect/', views.connect),
 ]
