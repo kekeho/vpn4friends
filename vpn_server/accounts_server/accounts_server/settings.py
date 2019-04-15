378a689dd4452af4c13a5e67bb017e4a5831ba98
Hiroki.T <hirodora@me.com>
2019-04-15 13:01:10 +0900

    del: remove db container

--- vpn_server/accounts_server/accounts_server/settings.py
+++ vpn_server/accounts_server/accounts_server/settings.py
@@ -75,15 +75,15 @@ WSGI_APPLICATION = 'accounts_server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'HOST': 'db',
+#         'PORT': 5432,
+#     }
+# }
 
 
 # Password validation
